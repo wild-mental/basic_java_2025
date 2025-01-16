fc1ed0814e7655d5fdf3adc5d73b14f0f39fb3e8
wild-mental <wild.mental@kidsnote.com>
2025-01-16 15:41:20 +0900

    Lombok 사용 방법 추가 설명

--- src/main/java/w3_6_oop/LombokUsageSample.java
+++ src/main/java/w3_6_oop/LombokUsageSample.java
@@ -1,11 +1,24 @@
 package w3_6_oop;
 
-import lombok.Data;
+import lombok.*;
+//import lombok.Value;
+//import lombok.experimental.FieldDefaults;
+//
+//import java.beans.BeanProperty;
 
-@Data
+// @Data
+@Getter @Setter
+@ToString
+//@EqualsAndHashCode
+//@NoArgsConstructor
+@RequiredArgsConstructor
+// @AllArgsConstructor
+// @Builder
+@Value  // final private 일괄 적용
 public class LombokUsageSample {
-    private int age = 20;
-    private boolean employed = false;
+    @NonNull
+    final private int age;
+    final private boolean employed;
 
     /* lombok 을 사용하면 아래와 같은 메서드를 수동 생성할 필요가 없어짐
     public void setEmployed(boolean employed) {
@@ -17,14 +30,24 @@ public class LombokUsageSample {
     }
     */
 
+    // @BeanProperty
     public static void main(String[] args) {
-        LombokUsageSample sul = new LombokUsageSample();
-        System.out.println(sul.getAge());
-        sul.setAge(30);
-        System.out.println(sul.getAge());
+//        LombokUsageSample sul = new LombokUsageSample();
+//        System.out.println(sul.getAge());
+//        sul.setAge(30);
+//        System.out.println(sul.getAge());
+//
+//        System.out.println(sul.isEmployed());
+//        sul.setEmployed(true);
+//        System.out.println(sul.isEmployed());
 
-        System.out.println(sul.isEmployed());
-        sul.setEmployed(true);
-        System.out.println(sul.isEmployed());
+        // System.out.println(sul);
+
+//        LombokUsageSample myLombokObj1 = new LombokUsageSample(
+//            1
+//        );
+        LombokUsageSample myLombokObj2 = new LombokUsageSample(
+            1, false
+        );
     }
 }
